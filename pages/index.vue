515562414ab36b3daea5093bf6b9cff65eaa867d
Helen Humphrey <helen@helenhumphrey.co>
2019-07-10 21:59:31 +0100

    chore: add prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-
     <component :is="getLayout" :allitems="allBlogPosts"></component>
 </template>
 
@@ -7,63 +6,60 @@
 import BaelGrid from "~/components/BaelGrid";
 import FullGrid from "~/components/FullGrid";
 export default {
-    watchQuery: ['page'],
+    watchQuery: ["page"],
 
-   transition (to, from) {
-     
-    if (!from) return 'fade'
-    return +to.query.page > +from.query.page ? 'slide-right' : 'slide-left'
-  },
-  name: "Index",
-  components: { BaelGrid,FullGrid },
-  data() {
-    return {};
-  },
-  methods: {},
-
-  computed: {
-    allBlogPosts() {
-      return this.$store.state.blogPosts;
+    transition(to, from) {
+        if (!from) return "fade";
+        return +to.query.page > +from.query.page ? "slide-right" : "slide-left";
     },
-    getLayout() {
-if (this.$store.state.siteInfo.altlayout == false ) {
-  return 'BaelGrid'
-} else if (this.$store.state.siteInfo.altlayout == true ) {
-  return 'FullGrid'
-}
+    name: "Index",
+    components: { BaelGrid, FullGrid },
+    data() {
+        return {};
+    },
+    methods: {},
 
+    computed: {
+        allBlogPosts() {
+            return this.$store.state.blogPosts;
+        },
+        getLayout() {
+            if (this.$store.state.siteInfo.altlayout == false) {
+                return "BaelGrid";
+            } else if (this.$store.state.siteInfo.altlayout == true) {
+                return "FullGrid";
+            }
+        }
     }
-  }
 };
 </script>
 
 <style>
-
 .browse a {
-  width: 100%;
+    width: 100%;
 }
 .search:focus {
-  outline: none;
+    outline: none;
 }
 .footer__heading {
-  text-transform: uppercase;
+    text-transform: uppercase;
 }
 nav .r {
-  grid-gap: 0;
+    grid-gap: 0;
 }
 .r.full-height {
-  grid-gap: 0;
+    grid-gap: 0;
 }
 @media only screen and (max-width: 40rem) {
-  .xs-collapse {
-    visibility: hidden;
-    visibility: collapse;
-    border: 0 !important;
-    border-color: none !important;
-    padding: 0 !important;
-  }
-  .xs-visible {
-    visibility: visible;
-  }
+    .xs-collapse {
+        visibility: hidden;
+        visibility: collapse;
+        border: 0 !important;
+        border-color: none !important;
+        padding: 0 !important;
+    }
+    .xs-visible {
+        visibility: visible;
+    }
 }
 </style>
